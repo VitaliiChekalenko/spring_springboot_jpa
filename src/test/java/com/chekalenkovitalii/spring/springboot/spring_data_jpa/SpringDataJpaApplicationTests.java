f3c40ed4f3ff33bc9f0edfb8f6ffdeebe0d12cbf
Chekalenko Vitaliy <chekalenkovitaliy@gmail.com>
2021-01-16 06:34:21 +0200

    Spring.SpringBoot JPA l.100

---
+++ src/test/java/com/chekalenkovitalii/spring/springboot/spring_data_jpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chekalenkovitalii.spring.springboot.spring_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
